5ca5b5575d30b0f374625f9a756a246962ecc9ab
matheus <santos95.mat@gmail.com>
2022-05-09 18:30:59 -0300

    massive

--- src/database/connection.js
+++ src/database/connection.js
@@ -3,26 +3,26 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-// export const connection = new SEQUELIZE(
-//     process.env.DB_BASE,
-//     process.env.DB_USER,
-//     process.env.DB_PASS,
-//     {
-//         host: process.env.DB_LOCAL,
-//         port: 5432,
-//         dialect: 'postgres'
-//     }
-// );
-
 export const connection = new SEQUELIZE(
-'postgres://redesocial_user:ICWOc1Rv78d6ykxutdedzg3pMwCJBraE@dpg-c9rgh8sgqg450d9v32gg-a.oregon-postgres.render.com/redesocial',
+    process.env.DB_BASE,
+    process.env.DB_USER,
+    process.env.DB_PASS,
     {
-        dialect: 'postgres',
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false
-            }
-        }
+        host: process.env.DB_LOCAL,
+        port: 5432,
+        dialect: 'postgres'
     }
 );
+
+// export const connection = new SEQUELIZE(
+// 'postgres://redesocial_user:ICWOc1Rv78d6ykxutdedzg3pMwCJBraE@dpg-c9rgh8sgqg450d9v32gg-a.oregon-postgres.render.com/redesocial',
+//     {
+//         dialect: 'postgres',
+//         dialectOptions: {
+//             ssl: {
+//                 require: true,
+//                 rejectUnauthorized: false
+//             }
+//         }
+//     }
+// );
